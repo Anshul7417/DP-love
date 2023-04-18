37579893f87450b6e6ed9fff560ae3d163db6f27
Anshul Tripathi <tripathianshul06@gmail.com>
2023-04-18 23:53:17 +0530

    started again

---
+++ waystoreachnthstair.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+int solve(int nStairs, int i){
+    if(i==nStairs) return 1;
+
+    if(i>nStairs) return 0;
+
+
+    return solve(nStairs, i+1) + solve(nStairs, i+2);
+}
+int countDistinctWays(int nStairs) {
+    solve(nStairs, 0);
+}
