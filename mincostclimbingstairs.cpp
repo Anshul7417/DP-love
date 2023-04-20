bfe29071aa65a30960227b0b5ba4c4a691ebe31f
Anshul Tripathi <tripathianshul06@gmail.com>
2023-04-20 23:55:49 +0530

    done 1d

--- mincostclimbingstairs.cpp
+++ mincostclimbingstairs.cpp
@@ -1,17 +1,22 @@
-class Solution {
+class Solution
+{
 public:
-    int solve(vector<int>& cost, int n){
-        if(n==0) return cost[0];
+    int solve(vector<int> &cost, int n)
+    {
+        if (n == 0)
+            return cost[0];
 
-        if(n==1) return cost[1];
+        if (n == 1)
+            return cost[1];
 
-        int ans = cost[n]+min(solve(cost, n-1), solve(cost, n-2));
+        int ans = cost[n] + min(solve(cost, n - 1), solve(cost, n - 2));
 
         return ans;
     }
-    int minCostClimbingStairs(vector<int>& cost) {
+    int minCostClimbingStairs(vector<int> &cost)
+    {
         int n = cost.size();
-        int ans = min(solve(cost, n-1), solve(cost, n-2));
+        int ans = min(solve(cost, n - 1), solve(cost, n - 2));
         return ans;
     }
 };
@@ -19,25 +24,81 @@ public:
 
 
 
-class Solution {
+
+class Solution
+{
 public:
-    int solve(vector<int>& cost, int n, vector<int> dp){
-        if(n==0) return cost[0];
+    int solve(vector<int> &cost, int n, vector<int> dp)
+    {
+        if (n == 0)
+            return cost[0];
 
-        if(n==1) return cost[1];
+        if (n == 1)
+            return cost[1];
 
-        if(dp[n]!=1) return dp[n];
+        if (dp[n] != 1)
+            return dp[n];
 
-        dp[n] = cost[n]+min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+        dp[n] = cost[n] + min(solve(cost, n - 1, dp), solve(cost, n - 2, dp));
 
         return dp[n];
     }
-    
-    int minCostClimbingStairs(vector<int>& cost) {
+
+    int minCostClimbingStairs(vector<int> &cost)
+    {
         int n = cost.size();
 
-        vector<int> dp(n+1, -1);
-        int ans = min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+        vector<int> dp(n + 1, -1);
+        int ans = min(solve(cost, n - 1, dp), solve(cost, n - 2, dp));
         return ans;
     }
 };
+
+
+
+
+
+
+
+int solve3(vector<int> &cost, int n)
+{
+    // step1: creation of dp array
+    vector<int> dp(n + 1);
+    // step2: base case analyse
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+    // 3rd step
+    Ifor(int i = 2; i < n; i++)
+    {
+        dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+    }
+    return min(dp[n - 1], dp[n - 2]);
+}
+
+
+
+
+
+
+
+// space optimised
+
+    class Solution {
+    public:
+
+    int solve(vector<int>& cost, int n) {
+    int prev2 = cost [0];
+    int prev1 = cost [1];
+    for(int i=2; i<n; i++) {
+    int curr = cost[i] + min(prev1, prev2);
+    prev2 = prev1;
+    prev1 = curr;
+    
+    }
+    return min(prev1, prev2);
+    }
+    
+        int minCostClimbingStairs(vector<int>& cost) {
+          return solve(cost,cost.size());  
+        }
+    };
---
+++ minnoofcoins.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+int solve(vector<int> &num, int x){
+    if(x==0) return 0;
+
+    if(x<0) return INT_MAX;
+
+    if(dp[x]!=-1) return -1;
+
+    int mini = INT_MAX;
+
+    for(int i=0; i<num.size(); i++){
+        int ans= solve(num, x-num[i]);
+        if(ans!=INT_MAX) mini =min(mini, 1+ans);
+    }
+
+
+    dp[x]=mini;
+    return dp[x];
+} 
+int minimumElements(vector<int> &num, int x)
+{
+    vector<int> dp(x+1, -1);
+    int ans=solve(num, x, dp);
+    if(ans==INT_MAX) return -1;
+
+    return ans;
+}
+
+
+
+
+
+
+
+int solve(vector<int> &num, int x, vector<int> & dp){
+    if(x==0) return 0;
+
+    if(x<0) return INT_MAX;
+
+    if(dp[x]!=-1) return dp[x];
+
+    int mini = INT_MAX;
+
+    for(int i=0; i<num.size(); i++){
+        int ans= solve(num, x-num[i], dp);
+        if(ans!=INT_MAX) mini =min(mini, 1+ans);
+    }
+
+
+    dp[x]=mini;
+    return dp[x];
+} 
+
+
+
+
+
+    int solveTab(vector<int> &num, int x) {
+    vector<int> dp(x+1, INT_MAX);
+    dp [0] = 0;
+    for(int i=1; i<=x; i++) {
+    //im trying to solve for every amount figure from 1 to x
+    for(int j = 0; j<num.size(); j++) {
+      if (i - num[j] >= 0 && dp[i - num[j]] != INT_MAX) {
+        dp[i] = min(dp[i], 1 + dp[i - num[j]]);
+      }
+    }
+    }
+
+    if (dp[x] == INT_MAX)
+      return -1;
+    return dp[x];
+    }
+int minimumElements(vector<int> &num, int x)
+{
+//     vector<int> dp(x+1, INT_MAX);
+//     int ans=solve(num, x, dp);
+    int ans=solveTab(num, x);
+    if(ans==INT_MAX) return -1;
+
+    return ans;
+}
